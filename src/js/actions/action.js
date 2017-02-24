aa687d25df27bbf6eb392e53de92422a99da8250
Gethyl <gethylgeorge@gmail.com>
2017-02-24 06:02:21 +0530

    Clean up dead code

--- src/js/actions/action.js
+++ src/js/actions/action.js
@@ -26,48 +26,6 @@ export const initialItems = (res) => ({
 })
 
 /***************************************************************************************** */
-/* Async action items using Axios														   */
-/***************************************************************************************** */
-export const addNewItem = (id,item) => {
-	return (dispatch) => {
-		let postData = {
-				id:id+1,
-				item:item,
-				completed:false
-		     }
-		axios.post("http://localhost:3000/api/additem",postData)
-			 .then(res=>{
-				dispatch(AddItem(postData))
-		     })
-	}	
-}
-
-export const markItemComplete = (id,completedFlag) => {
-	return (dispatch) => {
-		let postData = {
-				id:id,
-				completed:completedFlag
-		     }
-		axios.post("http://localhost:3000/api/markcomplete",postData)
-			 .then(res=>{
-				console.dir(res.data.result)
-				dispatch(completeItem(postData))
-		     })
-	}	
-}
-
-export const loadInitialData = () => {
-	return (dispatch) => {
-		axios.get("http://localhost:3000/api/getallitems")
-			 .then(res=>{
-				console.dir(res.data.result)
-				let newArray = res.data.result.map(x=>{return {id:x.itemId, text:x.item,completed:x.completed}})
-				dispatch(initialData(newArray))
-				})
-	}	
-}
-
-/***************************************************************************************** */
 /* Async Action items using - Sockets													   */
 /***************************************************************************************** */
 export const loadInitialDataSocket = (socket) => {
